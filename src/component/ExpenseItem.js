c6b3a88c6de757b20483f241d8788b5636c3d40d
unknown <sabamoin419@gmail.com>
2024-09-06 00:06:51 +0530

    Expense learned ClassName with Css

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,11 +1,14 @@
-
+import "./ExpenseItem.css"
 function ExpenseItem(){
     return(
-        <div>
-            <p>Food - $5</p>
-            <p>Petrol - $20</p>
-            <p>Movie - $10</p>
-        </div>
+        <div className="expense-item">
+      <div>15 August 2023</div>
+      <div className="expense-item__location">Delhi</div>
+      <div className="expense-item__description">
+        <h2>Book</h2>
+        <div className="expense-item__price">$10</div>
+      </div>
+    </div>
     )
 }
 export default ExpenseItem;
