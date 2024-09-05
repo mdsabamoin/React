f8a1e385cd377a963ebd1ceb6e39b5be492f8a23
unknown <sabamoin419@gmail.com>
2024-09-06 02:14:06 +0530

    Introduction to props

--- src/App.js
+++ src/App.js
@@ -5,7 +5,30 @@ function App() {
     <div>
       <h1>Let's get started</h1>
       <p>I am Learning React</p>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 7, 15)}
+        title="Insurance"
+        price="50"
+        location="Bangalore"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 3, 25)}
+        title="Book"
+        price="20"
+        location="Delhi"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 10, 11)}
+        title="Pen"
+        price="5"
+        location="Hyderabad"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 1, 14)}
+        title="Laptop"
+        price="200"
+        location="Mumbai"
+      ></ExpenseItem>
     </div>
   );
 }
