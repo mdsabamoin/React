9609e02a873e362e13d2419a74e83de590fdc6aa
unknown <sabamoin419@gmail.com>
2024-09-05 23:07:45 +0530

    First Custom component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import ExpenseItem from "./component/ExpenseItem";
 
 function App() {
   return (
     <div>
       <h1>Let's get started</h1>
       <p>I am Learning React</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
