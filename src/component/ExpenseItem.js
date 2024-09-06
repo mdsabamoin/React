a4ecc54b1edda5063d804cfb8df29d72c2e3d408
unknown <sabamoin419@gmail.com>
2024-09-06 23:54:52 +0530

    map in react

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,8 +1,8 @@
 import "./ExpenseItem.css"
 function ExpenseItem(props){
     return(
-        <div className="expense-item">
-      <div>{props.date}</div>
+   <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__location">{props.location}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
