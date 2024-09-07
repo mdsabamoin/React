565d12a7845d869e73fa54b1fe0f1390ff74bdce
unknown <sabamoin419@gmail.com>
2024-09-08 01:14:48 +0530

    Learnt Date format

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,8 +1,16 @@
 import "./ExpenseItem.css"
 function ExpenseItem(props){
+  let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  let month = months[props.date.getMonth()];
+  let year = props.date.getFullYear();
+  let date = props.date.getDate();
     return(
    <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{date}</div>
+      </div>
       <div className="expense-item__location">{props.location}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
