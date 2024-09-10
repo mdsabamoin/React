2d415e09a02b6a2d52b14f6d0f6c39174ee6b165
unknown <sabamoin419@gmail.com>
2024-09-10 22:05:09 +0530

    Evenhandler in react

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -2,12 +2,16 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate.js"
 import Card from "./Card"
 function ExpenseItem(props) {
+  function handlebutton(event){
+    console.log("Button clicked");
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
+        <button onClick={handlebutton}></button>
       </div>
     </Card>
   );
