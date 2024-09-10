f280cdf9b7747d206358ad50852f9b45beb33f81
unknown <sabamoin419@gmail.com>
2024-09-10 23:01:48 +0530

    usestate learnt in React

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,17 +1,19 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate.js"
+import React , {useState} from "react";
 import Card from "./Card"
 function ExpenseItem(props) {
+  const [title , setTitle] = useState(props.title);
   function handlebutton(event){
-    console.log("Button clicked");
+    setTitle("New Title");
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
-        <button onClick={handlebutton}></button>
+        <button  width="20"onClick={handlebutton}></button>
       </div>
     </Card>
   );
